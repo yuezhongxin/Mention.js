9924ea19a58fa50c1d5c71fb9dc2594232941fbc
蟋蟀 <xishuai@cnblogs.com>
2015-09-16 15:37:57 +0800

    Remove data-value

--- src/scripts/mention-typeahead.js
+++ src/scripts/mention-typeahead.js
@@ -51,7 +51,7 @@
     constructor: Typeahead
 
   , select: function () {
-      var val = this.$menu.find('.active').attr('data-value')
+      var val = this.$menu.find('.active').text()
       this.$element
         .val(this.updater(val + " "))
         .change()
@@ -164,7 +164,7 @@
       var that = this
 
       items = $(items).map(function (i, item) {
-        i = $(that.options.item).attr('data-value', item)
+        i = $(that.options.item).text(item)
         i.find('a').html(that.highlighter(item))
         return i[0]
       })
